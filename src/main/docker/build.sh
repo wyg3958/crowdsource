a9dd7d8a9c4aab654e066173f9363e6a455adf2d
henning.gross <igaffai@gmail.com>
2014-10-29 22:25:31 +0100

    - remove debug-curls - cp instead of move

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd ${project.build.directory}/docker
-mv ${project.build.directory}/${project.build.finalName}.jar .
+cp ${project.build.directory}/${project.build.finalName}.jar .
 
 docker build -t "asideas/crowdsource:latest" .
--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 docker run -p ${it.application.port}:8080 -d --name="crowdsource" "asideas/crowdsource:latest"
-curl http://127.0.0.1:${it.application.port}/hello
-curl http://127.0.0.1:${it.application.port}

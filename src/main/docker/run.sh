c0d2924c6fa2047e53311df96e08c98f7a8672fe
henning.gross <igaffai@gmail.com>
2014-10-29 22:20:03 +0100

    debug-curls

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 docker run -p ${it.application.port}:8080 -d --name="crowdsource" "asideas/crowdsource:latest"
+curl http://localhost:${it.application.port}/hello
+curl http://localhost:${it.application.port}

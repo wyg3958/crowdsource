ba4a9ba5ded2f1f5a6d00d0e5a2ad7994328c412
henning.gross <igaffai@gmail.com>
2014-10-29 22:29:59 +0100

    - make sure app is booted

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 docker run -p ${it.application.port}:8080 -d --name="crowdsource" "asideas/crowdsource:latest"
+# wait a few seconds for app to boot
+sleep 10
+# curl as health check
 curl http://127.0.0.1:${it.application.port}

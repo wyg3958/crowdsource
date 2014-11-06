3c75cfb02c803605f9a5dc6242bc1c456f9ca95a
sweller <stefan.weller@asideas.de>
2014-11-06 16:00:57 +0100

    - aws key permissions - dont waste time waiting for service (poll)

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -2,6 +2,19 @@
 
 docker run -p ${it.application.port}:8080 -d --name="crowdsource" "asideas/crowdsource:latest"
 # wait a few seconds for app to boot
-sleep 15
+
 # curl as health check
-curl http://127.0.0.1:${it.application.port}
+echo "CHECKING AVAILABILITY OF LOCALLY LAUNCHED SERVICE..."
+for i in {1..30}
+do
+ REQUEST_RESULT=`curl -s -I http://localhost:${it.application.port} | grep "200 OK"`
+
+ if [ "$REQUEST_RESULT" != "" ]; then
+  echo "REQUEST TO SERVICE SUCCESSFULLY RETURNED WITH CODE 200"
+  STATUS=0
+  break;
+ else
+  echo "RETURN CODE FROM SERVICE INCORRECT - WAITING..."
+ fi
+ sleep 1
+done

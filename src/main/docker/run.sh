8bd4e431bcd31999748d3560ebd9669e677170a8
sweller <stefan.weller@asideas.de>
2014-11-06 15:53:07 +0100

    add private AWS key

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -2,6 +2,6 @@
 
 docker run -p ${it.application.port}:8080 -d --name="crowdsource" "asideas/crowdsource:latest"
 # wait a few seconds for app to boot
-sleep 10
+sleep 15
 # curl as health check
 curl http://127.0.0.1:${it.application.port}

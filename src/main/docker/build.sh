6e96b6ebef2bb92dff0eeee7e44453f4efd8d509
sweller <stefan.weller@asideas.de>
2014-11-12 16:04:15 +0100

    continue debugging

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # clean
-docker kill crowdsource
-docker rm crowdsource
-docker rmi 'asideas/crowdsource'
+docker -H tcp://10.1.42.1:2375 kill crowdsource
+docker -H tcp://10.1.42.1:2375 rm crowdsource
+docker -H tcp://10.1.42.1:2375 rmi 'asideas/crowdsource'
 
 # build
 cd ${project.build.directory}/docker
 cp ${project.build.directory}/${project.build.finalName}.jar .
 
-docker build -t "asideas/crowdsource" .
+docker build -H tcp://10.1.42.1:2375 -t "asideas/crowdsource" .

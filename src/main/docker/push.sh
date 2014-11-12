ade005c5ac6adea1722d7b0dcbde22b6ac4bbaab
sweller <stefan.weller@asideas.de>
2014-11-12 16:13:32 +0100

    continue debugging

--- src/main/docker/push.sh
+++ src/main/docker/push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker login -e crowdsource@asideas.de -p ideas987 -u asjenkins
-docker push asideas/crowdsource
+docker -H tcp://10.1.42.1:2375 login -e crowdsource@asideas.de -p ideas987 -u asjenkins
+docker -H tcp://10.1.42.1:2375 push asideas/crowdsource

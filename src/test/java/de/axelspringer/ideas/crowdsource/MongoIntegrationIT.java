11c92a277dbeee89faa276241ab1dcc392162026
henning.gross <igaffai@gmail.com>
2014-11-15 01:19:58 +0100

    why start a webapp for the mongo test?

--- src/test/java/de/axelspringer/ideas/crowdsource/MongoIntegrationIT.java
+++ src/test/java/de/axelspringer/ideas/crowdsource/MongoIntegrationIT.java
@@ -1,26 +1,23 @@
 package de.axelspringer.ideas.crowdsource;
 
+import de.axelspringer.ideas.crowdsource.config.MongoDBConfig;
 import de.axelspringer.ideas.crowdsource.model.Hello;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@IntegrationTest
-@WebAppConfiguration
-@SpringApplicationConfiguration(classes = CrowdSourceConfig.class)
+@ContextConfiguration(classes = MongoDBConfig.class)
 public class MongoIntegrationIT {
 
     @Autowired

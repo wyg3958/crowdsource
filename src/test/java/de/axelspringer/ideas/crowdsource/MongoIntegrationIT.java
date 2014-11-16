03db83eb6d464b3d83fd7675233dff67ac60dfd9
henning.gross <igaffai@gmail.com>
2014-11-16 08:21:13 +0100

    move test init to before-method

--- src/test/java/de/axelspringer/ideas/crowdsource/MongoIntegrationIT.java
+++ src/test/java/de/axelspringer/ideas/crowdsource/MongoIntegrationIT.java
@@ -2,17 +2,16 @@ package de.axelspringer.ideas.crowdsource;
 
 import de.axelspringer.ideas.crowdsource.config.MongoDBConfig;
 import de.axelspringer.ideas.crowdsource.model.Hello;
+import de.axelspringer.ideas.crowdsource.testsupport.CrowdSourceTestConfig;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -21,36 +20,41 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = MongoIntegrationIT.MongoIntegrationITConfig.class)
+@ContextConfiguration(classes = MongoIntegrationIT.MongoITConfig.class)
 public class MongoIntegrationIT {
 
     @Autowired
     private MongoOperations mongoOperations;
 
-    @Test
-    public void shouldConnectToMongoDBAndFindOneTestObject() {
-
-        Hello hello = new Hello();
-        hello.setMessage("hi from mongo!");
-
+    @Before
+    public void initDB() {
+        // try to remove all Hellos first (which is also a connection check)
         try {
-            mongoOperations.findAllAndRemove(new Query(), Hello.class);
-            mongoOperations.save(hello);
+            mongoOperations.findAllAndRemove(null, Hello.class);
         } catch (DataAccessResourceFailureException e) {
             Assert.fail("MongoDB not available. Did you start and configure a valid Mongo instance?");
         }
-
-        final List<Hello> hellos = mongoOperations.findAll(Hello.class);
-
-        assertEquals("Test message not found in given MongoDB.", 1, hellos.size());
     }
 
+    @Test
+    public void shouldConnectToMongoDBAndFindOneTestObject() {
+
+        // new hello instance
+        Hello hello = new Hello();
+        hello.setMessage("hi from mongo!");
+
+        // save to db
+        mongoOperations.save(hello);
+
+        // query db
+        final List<Hello> hellos = mongoOperations.findAll(Hello.class);
+
+        // assert hello instance is found
+        assertEquals("Test message count != 1.", 1, hellos.size());
+    }
+
+    @Import({CrowdSourceTestConfig.class, MongoDBConfig.class})
     @Configuration
-    @Import(MongoDBConfig.class)
-    static class MongoIntegrationITConfig {
-        @Bean
-        public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
-            return new PropertyPlaceholderConfigurer();
-        }
+    static class MongoITConfig {
     }
 }

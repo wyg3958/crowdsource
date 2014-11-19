8f2765c85f1f0e6a248912bbdc66d635afdb4645
henning.gross <igaffai@gmail.com>
2014-11-19 17:52:07 +0100

    - basic integration of spring security

---
+++ src/main/java/de/axelspringer/ideas/crowdsource/config/security/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package de.axelspringer.ideas.crowdsource.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Adds and configures Spring Security Servlet Filter
+ */
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}

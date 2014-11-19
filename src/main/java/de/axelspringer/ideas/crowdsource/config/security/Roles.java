6487ab3992e1d830ec0679b6a21f9a1a92e779d6
henning.gross <igaffai@gmail.com>
2014-11-19 17:52:07 +0100

    - basic integration of spring security

---
+++ src/main/java/de/axelspringer/ideas/crowdsource/config/security/Roles.java
@@ -0,0 +1,19 @@
+package de.axelspringer.ideas.crowdsource.config.security;
+
+public interface Roles {
+
+    /**
+     * Anonymous user. An user that is neither logged in nor in a trusted network.
+     */
+    String ROLE_UNTRUSTED_ANONYMOUS = "ROLE_UNTRUSTED_ANONYMOUS";
+
+    /**
+     * User that is not logged in but in a trusted network.
+     */
+    String ROLE_TRUSTED_ANONYMOUS = "ROLE_TRUSTED_ANONYMOUS";
+
+    /**
+     * User that is logged in.
+     */
+    String ROLE_USER = "ROLE_USER";
+}
---
+++ src/main/java/de/axelspringer/ideas/crowdsource/config/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package de.axelspringer.ideas.crowdsource.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Configuration for Spring Security
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("test").password("test").roles(Roles.ROLE_USER);
+    }
+}

69cb6465fabd80056a264a2db12bb629996eb89d
henning.gross <igaffai@gmail.com>
2014-11-21 15:55:54 +0100

    max time

--- src/main/java/de/axelspringer/ideas/crowdsource/config/security/SecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package de.axelspringer.ideas.crowdsource.config.security;
-
-/**
- * Adds and configures Spring Security Servlet Filter
- */
-public class SecurityInitializer {
-}
-//extends AbstractSecurityWebApplicationInitializer {}

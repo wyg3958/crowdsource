294e35da51d7720b88314874db13ee3133a33cf2
Timo Ulich <timo.ulich@asideas.de>
2014-11-26 08:23:57 +0100

    redirect to signup-success on success

--- src/main/resources/public/app/crowdsource.js
+++ src/main/resources/public/app/crowdsource.js
@@ -12,6 +12,10 @@ angular.module('crowdsource', ['ngRoute', 'ngResource', 'ngMessages'])
         .when('/signup', {
             templateUrl: 'app/user/signup.html',
             controller: 'SignupController as signup'
+        })
+        .when('/signup/:email/success', {
+            templateUrl: 'app/user/signup-success.html',
+            controller: 'SignupSuccessController as signupSuccess'
         });
 
     // we can only activate this, once you can reload the page with the path not being /

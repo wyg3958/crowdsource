2cad255406bff47e3009c58f166750d0ba090dd1
henning.gross <igaffai@gmail.com>
2014-12-02 10:50:07 +0100

    moved scss to non-source-folder

--- crowdsource-frontend/gulpfile.js
+++ crowdsource-frontend/gulpfile.js
@@ -5,34 +5,21 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 
-var BASE_DIR = 'src/main/resources/public';
-var JS_FILES = BASE_DIR + '/app/**/*.js';
+var BASE_DIR = 'src/main/scss';
 
-gulp.task('sass', function() {
-    return gulp.src(BASE_DIR + '/app/crowdsource.scss')
+// sass compile task
+gulp.task('sass', function () {
+    return gulp.src(BASE_DIR + '/crowdsource.scss')
         .pipe(sourcemaps.init())
-            .pipe(sass({ outputStyle: 'compressed', includePaths: ['bower_components'] }))
+        .pipe(sass({outputStyle: 'compressed', includePaths: ['bower_components']}))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(BASE_DIR));
+        .pipe(gulp.dest("target/classes/public/css"));
 });
 
-gulp.task('watch', function() {
-    return gulp.watch(BASE_DIR + '/app/**/*.scss', ['sass']);
+// exec "node_modules/.bin/gulp watch" or "npn run watch" for local development. will compile sass on changes.
+gulp.task('watch', function () {
+    return gulp.watch(BASE_DIR + '/**/*.scss', ['sass']);
 });
 
-// Uncomment this for building a single minified javascript file
-//gulp.task('js', function() {
-//    return gulp.src(JS_FILES)
-//        .pipe(sourcemaps.init())
-//            .pipe(ngAnnotate())
-//            .pipe(uglify())
-//            .pipe(concat('crowdsource.min.js'))
-//        .pipe(sourcemaps.write('.'))
-//        .pipe(gulp.dest(BASE_DIR));
-//});
-//
-//gulp.task('watch', function() {
-//    return gulp.watch(JS_FILES, ['js']);
-//});
-
+// sass is the default gulp task
 gulp.task('default', ['sass']);

b580f40e81bba786787550dd7bbcae69aa5a6445
henning.gross <igaffai@gmail.com>
2014-12-03 18:51:37 +0100

    password

--- crowdsource-blackboxtests/src/test/java/de/axelspringer/ideas/crowdsource/AuthenticationIT.java
+++ crowdsource-blackboxtests/src/test/java/de/axelspringer/ideas/crowdsource/AuthenticationIT.java
@@ -54,7 +54,7 @@ public class AuthenticationIT {
 
     @Test
     public void authorizedRequest() throws IOException {
-        TokenResponse tokenResponse = requestToken("crowdsource@axelspringer.de", "test");
+        TokenResponse tokenResponse = requestToken("crowdsource@axelspringer.de", "einEselGehtZumBaecker!");
         assertThat(tokenResponse.getAccessToken(), is(notNullValue()));
 
         ResponseEntity<Hello> response = getHello(tokenResponse.getAccessToken());

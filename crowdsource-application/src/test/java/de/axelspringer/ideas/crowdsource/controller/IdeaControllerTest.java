31b51f6e0d29554f743f342cf1e254677f807986
sweller <stefan.weller@asideas.de>
2014-12-09 17:13:04 +0100

    add basic test setup

--- crowdsource-application/src/test/java/de/axelspringer/ideas/crowdsource/controller/IdeaControllerTest.java
+++ crowdsource-application/src/test/java/de/axelspringer/ideas/crowdsource/controller/IdeaControllerTest.java
@@ -1,7 +1,74 @@
 package de.axelspringer.ideas.crowdsource.controller;
 
-/**
- * Created by sweller on 09.12.14.
- */
+import de.axelspringer.ideas.crowdsource.model.persistence.UserEntity;
+import de.axelspringer.ideas.crowdsource.repository.IdeaRepository;
+import de.axelspringer.ideas.crowdsource.repository.UserRepository;
+import de.axelspringer.ideas.crowdsource.service.UserActivationService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.annotation.Resource;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = IdeaControllerTest.Config.class)
 public class IdeaControllerTest {
+
+    @Autowired
+    private IdeaController ideaController;
+
+    @Autowired
+    private IdeaRepository ideaRepository;
+
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        reset(ideaRepository);
+    }
+
+    @Test
+    public void shouldReturnSuccessfullyOnSave(){
+
+    }
+
+
+
+    @Configuration
+    @EnableWebMvc
+    static class Config {
+
+        @Bean
+        public IdeaController ideaController() {
+            return new IdeaController();
+        }
+
+        @Bean
+        public IdeaRepository ideaRepository() {
+            return mock(IdeaRepository.class);
+        }
+    }
 }

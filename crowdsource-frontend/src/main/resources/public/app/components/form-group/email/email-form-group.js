fbc2f3b60e18accf677686f44efc4898febd8d7b
Timo Ulich <timo.ulich@asideas.de>
2014-12-11 10:34:47 +0100

    add documentation

--- crowdsource-frontend/src/main/resources/public/app/components/form-group/email/email-form-group.js
+++ crowdsource-frontend/src/main/resources/public/app/components/form-group/email/email-form-group.js
@@ -2,7 +2,16 @@ angular.module('crowdsource')
 
     .value('emailDomain', '@axelspringer.de')
 
-    .directive('emailFormGroup', function(emailDomain) {
+    /**
+     * A form group (label + input field) for the crowdsource email input where the user
+     * only has to enter the local part of the email address
+     *
+     * Label
+     * +----------------+------------------+
+     * |        foo.bar | @axelspringer.de |
+     * +----------------+------------------+
+     */
+    .directive('emailFormGroup', function (emailDomain) {
         return {
             restrict: 'E',
             require: '^form',
@@ -11,25 +20,31 @@ angular.module('crowdsource')
                 fieldName: '@'
             },
             templateUrl: 'app/components/form-group/email/email-form-group.html',
-            link: function(scope, element, attributes, form) {
+            link: function (scope, element, attributes, form) {
                 scope.form = form;
                 scope.EMAIL_DOMAIN = emailDomain;
             }
         };
     })
 
-    .directive('localEmailPartInput', function(emailDomain) {
+    /**
+     * When applied to an <input type="email"> field, this causes the input's model to be
+     * enriched with the value of the emailDomain value service. The user only has to enter
+     * the local part of the email address (everything before the '@') and the model
+     * will automatically contain the full email address.
+     */
+    .directive('localEmailPartInput', function (emailDomain) {
         return {
             restrict: 'A',
             require: 'ngModel',
             link: function (scope, element, attributes, ngModel) {
-                ngModel.$parsers.push(function(value) {
+                ngModel.$parsers.push(function (value) {
                     if (value) {
                         return value + emailDomain;
                     }
                     return value;
                 });
-                ngModel.$formatters.push(function(value) {
+                ngModel.$formatters.push(function (value) {
                     if (value) {
                         return value.substr(0, value.indexOf(emailDomain));
                     }
@@ -39,6 +54,9 @@ angular.module('crowdsource')
         };
     })
 
+    /**
+     * Custom validator that does not allow the local email part to contain "_extern"
+     */
     .directive('nonExternalEmail', function () {
         return {
             restrict: 'A',

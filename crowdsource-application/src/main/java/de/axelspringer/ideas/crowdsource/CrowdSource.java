5d0625cf6a3840dc64d1ebfa573d759efe26052c
Timo Ulich <timo.ulich@asideas.de>
2014-12-16 16:19:45 +0100

    use spring boot 1.2.0.RELEASE and it's new features

--- crowdsource-application/src/main/java/de/axelspringer/ideas/crowdsource/CrowdSource.java
+++ crowdsource-application/src/main/java/de/axelspringer/ideas/crowdsource/CrowdSource.java
@@ -4,14 +4,13 @@ import de.axelspringer.ideas.crowdsource.config.MailConfig;
 import de.axelspringer.ideas.crowdsource.config.MongoDBConfig;
 import de.axelspringer.ideas.crowdsource.config.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Configuration
+@SpringBootApplication
 @Import({MongoDBConfig.class, SecurityConfig.class, MailConfig.class})
-@EnableAutoConfiguration
 @ComponentScan(excludeFilters = @ComponentScan.Filter(Configuration.class))
 public class CrowdSource {
 

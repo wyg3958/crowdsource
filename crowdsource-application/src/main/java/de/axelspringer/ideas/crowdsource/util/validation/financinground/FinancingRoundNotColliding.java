25688dcf083e9e217fccddcdd6c25152156c2da0
henning.gross <igaffai@gmail.com>
2015-01-10 14:51:05 +0100

    FinancingRound view-model and validator

---
+++ crowdsource-application/src/main/java/de/axelspringer/ideas/crowdsource/util/validation/financinground/FinancingRoundNotColliding.java
@@ -0,0 +1,28 @@
+package de.axelspringer.ideas.crowdsource.util.validation.financinground;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Checks if a given Email is eligible for a user account at crowdsource
+ */
+@Documented
+@Constraint(validatedBy = FinancingRoundNotCollidingValidator.class)
+@Target(TYPE)
+@Retention(RUNTIME)
+@ReportAsSingleViolation
+public @interface FinancingRoundNotColliding {
+
+    String message() default "eligible";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}

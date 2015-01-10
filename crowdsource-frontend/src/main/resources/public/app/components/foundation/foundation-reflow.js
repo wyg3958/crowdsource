95df6c8ec6b14d287bc71f9167a8f52a000b6a59
Timo Ulich <timo.ulich@asideas.de>
2015-01-10 12:06:27 +0100

    crowd-132: make foundation-reflow directive configurable to reflow only the given element types

--- crowdsource-frontend/src/main/resources/public/app/components/foundation/foundation-reflow.js
+++ crowdsource-frontend/src/main/resources/public/app/components/foundation/foundation-reflow.js
@@ -3,11 +3,17 @@ angular.module('crowdsource')
     .factory('Foundation', function($timeout) {
         var service = {};
 
-        service.reflow = function() {
+        service.reflow = function(reflowComponent) {
             // angular may not have fully completed it's digest cycle
             // -> delay the reflow for the next javascript cycle
             $timeout(function() {
-                $(document).foundation('reflow');
+                if (reflowComponent) {
+                    // only reflow certain foundation element types, e.g. 'slider'
+                    $(document).foundation(reflowComponent, 'reflow');
+                }
+                else {
+                    $(document).foundation('reflow');
+                }
             });
         };
 
@@ -18,14 +24,16 @@ angular.module('crowdsource')
         return {
             restrict: 'A',
             link: function(scope, elem, attrs) {
+                var reflowComponent = attrs.foundationReflow;
+
                 if (attrs.ngRepeat) {
                     // if this directive was applied on an ng-repeat element, reflow only after ng-repeat is done
                     if (scope.$last) {
-                        Foundation.reflow();
+                        Foundation.reflow(reflowComponent);
                     }
                 }
                 else {
-                    Foundation.reflow();
+                    Foundation.reflow(reflowComponent);
                 }
             }
         }

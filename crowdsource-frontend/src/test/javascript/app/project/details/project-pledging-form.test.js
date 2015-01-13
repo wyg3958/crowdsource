951b9fd894693932d38e575ffca3845c27f375bb
Timo Ulich <timo.ulich@asideas.de>
2015-01-13 11:01:53 +0100

    crowd-106: disable input field when slider is disabled

--- crowdsource-frontend/src/test/javascript/app/project/details/project-pledging-form.test.js
+++ crowdsource-frontend/src/test/javascript/app/project/details/project-pledging-form.test.js
@@ -20,11 +20,12 @@ describe('project pledging form', function () {
         return {
             root: root,
             slider: root.find('.range-slider'),
+            pledgeAmountField: root.find('[name="amount"]'),
             pledgeButton: root.find('button')
         };
     }
 
-    it("should disable the slider until the user budget is loaded", function() {
+    it("should disable the form until the user budget is loaded", function() {
 
         $scope.project = { $resolved: true, pledgeGoal: 100, pledgedAmount: 50 };
         spyOn(Authentication, 'isLoggedIn').and.returnValue(true);
@@ -34,13 +35,15 @@ describe('project pledging form', function () {
 
         expect(elements.slider).toHaveClass('disabled');
         expect(elements.pledgeButton).toBeDisabled();
+        expect(elements.pledgeAmountField).toBeDisabled();
 
         $httpBackend.flush();
 
         expect(elements.slider).not.toHaveClass('disabled');
+        expect(elements.pledgeAmountField).not.toBeDisabled();
     });
 
-    it("should disable the slider until the project details are loaded", function () {
+    it("should disable the form until the project details are loaded", function () {
 
         $scope.project = { $resolved: false };
         spyOn(Authentication, 'isLoggedIn').and.returnValue(true);
@@ -50,14 +53,16 @@ describe('project pledging form', function () {
         $httpBackend.flush();
 
         expect(elements.slider).toHaveClass('disabled');
+        expect(elements.pledgeAmountField).toBeDisabled();
 
         angular.copy({ $resolved: true, pledgeGoal: 100, pledgedAmount: 50 }, $scope.project);
         $scope.$digest();
 
         expect(elements.slider).not.toHaveClass('disabled');
+        expect(elements.pledgeAmountField).not.toBeDisabled();
     });
 
-    it("should disable the slider if the user has no budget", function () {
+    it("should disable the form if the user has no budget", function () {
 
         $scope.project = { $resolved: true, pledgeGoal: 100, pledgedAmount: 50 };
         spyOn(Authentication, 'isLoggedIn').and.returnValue(true);
@@ -67,9 +72,10 @@ describe('project pledging form', function () {
         $httpBackend.flush();
 
         expect(elements.slider).toHaveClass('disabled');
+        expect(elements.pledgeAmountField).toBeDisabled();
     });
 
-    it("should disable the slider if the user is not logged in", function () {
+    it("should disable the form if the user is not logged in", function () {
 
         $scope.project = { $resolved: true, pledgeGoal: 100, pledgedAmount: 50 };
         spyOn(Authentication, 'isLoggedIn').and.returnValue(false);
@@ -77,5 +83,6 @@ describe('project pledging form', function () {
         var elements = compileDirective();
 
         expect(elements.slider).toHaveClass('disabled');
+        expect(elements.pledgeAmountField).toBeDisabled();
     });
 });

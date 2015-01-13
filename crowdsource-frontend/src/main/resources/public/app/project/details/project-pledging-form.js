74f23f11fb94e4641b360beb6e2e8260e803e88d
Timo Ulich <timo.ulich@asideas.de>
2015-01-13 18:47:07 +0100

    crowd-106: add error messages when over-pledging

--- crowdsource-frontend/src/main/resources/public/app/project/details/project-pledging-form.js
+++ crowdsource-frontend/src/main/resources/public/app/project/details/project-pledging-form.js
@@ -8,32 +8,34 @@ angular.module('crowdsource')
             scope: {
                 project: '='
             },
-            controller: function(Project, User) {
+            controller: function(Project, User, RemoteFormValidation, $q) {
                 var vm = this;
 
                 vm.user = User.current();
 
                 vm.pledgeProject = function() {
                     vm.success = false;
+                    RemoteFormValidation.clearRemoteErrors(vm);
 
                     Project.pledge(vm.project.id, vm.pledge).$promise
                         .then(function() {
-                            // load the project again to update the project to the new state
-                            // (mainly to update the "backers" property, as this cannot be
-                            // easily identified on the client
-                            return Project.get(vm.project.id).$promise;
-                        })
-                        .then(function(updatedProject) {
-                            angular.copy(updatedProject, vm.project);
+                            // load the project and user again to update the project details view to the new state
+                            return reloadUserAndProject();
+                        },
+                        function(response) {
+                            RemoteFormValidation.applyServerErrorResponse(vm, vm.form, response);
 
+                            // most probably the project details or displayed user budget was outdated and the user "over-pledged"
+                            reloadUserAndProject();
+
+                            // return a rejected promise, or the .then() callback below will be called
+                            return $q.reject(response);
+                        })
+                        .then(function() {
                             vm.success = true;
-                            vm.user.budget -= vm.pledge.amount;
                             vm.pledge.amount = 0;
 
                             vm.form.$setPristine();
-                        })
-                        .catch(function() {
-                            alert('Zu hoher Finanzierungsbetrag oder nicht eingeloggt');
                         });
                 };
 
@@ -45,6 +47,23 @@ angular.module('crowdsource')
                     var remainingProjectGoal = vm.project.pledgeGoal - vm.project.pledgedAmount;
                     return Math.min(remainingProjectGoal, vm.user.budget);
                 };
+
+
+                function reloadUserAndProject() {
+                    // parallel execution of backend calls
+                    var promises = $q.all({
+                        project: Project.get(vm.project.id).$promise,
+                        user: User.current().$promise
+                    });
+
+                    // will be resolved when both calls are completed
+                    promises.then(function(resolvedPromises) {
+                        angular.copy(resolvedPromises.project, vm.project);
+                        angular.copy(resolvedPromises.user, vm.user);
+                    });
+
+                    return promises;
+                }
             }
         };
     });

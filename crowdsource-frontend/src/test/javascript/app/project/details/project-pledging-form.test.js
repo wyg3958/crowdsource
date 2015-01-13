766b8f58edde49a88a68c4bf715ce44a680215cd
Timo Ulich <timo.ulich@asideas.de>
2015-01-13 13:27:17 +0100

    crowd-106: add validation messages on amount input field and add karma tests

--- crowdsource-frontend/src/test/javascript/app/project/details/project-pledging-form.test.js
+++ crowdsource-frontend/src/test/javascript/app/project/details/project-pledging-form.test.js
@@ -20,59 +20,112 @@ describe('project pledging form', function () {
         return {
             root: root,
             slider: root.find('.range-slider'),
-            pledgeAmountField: root.find('[name="amount"]'),
+            pledgeAmount: new FormGroup(root.find('.form-controls-amount')),
             pledgeButton: root.find('button')
         };
     }
 
+    function expectValidationError(formGroup, violatedRule) {
+        expect(formGroup.getLabelContainer()).toHaveClass('error');
+        expect(formGroup.getLabel()).toHaveClass('ng-hide');
+        expect(formGroup.getErrorLabelsContainer()).not.toHaveClass('ng-hide');
+        expect(formGroup.getErrorLabelForRule(violatedRule)).toExist();
+    }
+
+    function expectNoValidationError(formGroup) {
+        expect(formGroup.getLabelContainer()).not.toHaveClass('error');
+        expect(formGroup.getLabel()).not.toHaveClass('ng-hide');
+        expect(formGroup.getErrorLabelsContainer()).toHaveClass('ng-hide');
+    }
+
+    function prepareMocks(data) {
+        $scope.project = data.project;
+        spyOn(Authentication, 'isLoggedIn').and.returnValue(data.isLoggedIn);
+        $httpBackend.expectGET('/user/current').respond(data.userResponse.statusCode, data.userResponse.body);
+    }
+
+
+    it("should add a pledge", function () {
+
+        prepareMocks({
+            project: { $resolved: true, id: 123, pledgeGoal: 100, pledgedAmount: 60 },
+            isLoggedIn: true,
+            userResponse: { statusCode: 200, body: { budget: 200 } }
+        });
+
+        var elements = compileDirective();
+        $httpBackend.flush();
+
+        elements.pledgeAmount.getInputField().val('40').trigger('input');
+        expectNoValidationError(elements.pledgeAmount);
+        expect(elements.pledgeButton).not.toBeDisabled();
+
+
+        $httpBackend.expectPOST('/project/123/pledge', { amount: 40 }).respond(200);
+        elements.pledgeButton.click();
+
+        $httpBackend.flush();
+    });
+
     it("should disable the form until the user budget is loaded", function() {
 
-        $scope.project = { $resolved: true, pledgeGoal: 100, pledgedAmount: 50 };
-        spyOn(Authentication, 'isLoggedIn').and.returnValue(true);
-        $httpBackend.expectGET('/user/current').respond(200, { budget: 20 });
+        prepareMocks({
+            project: { $resolved: true, id: 123, pledgeGoal: 100, pledgedAmount: 50 },
+            isLoggedIn: true,
+            userResponse: { statusCode: 200, body: { budget: 20 } }
+        });
 
         var elements = compileDirective();
 
         expect(elements.slider).toHaveClass('disabled');
         expect(elements.pledgeButton).toBeDisabled();
-        expect(elements.pledgeAmountField).toBeDisabled();
+        expect(elements.pledgeAmount.getInputField()).toBeDisabled();
+        expectNoValidationError(elements.pledgeAmount);
 
         $httpBackend.flush();
 
         expect(elements.slider).not.toHaveClass('disabled');
-        expect(elements.pledgeAmountField).not.toBeDisabled();
+        expect(elements.pledgeAmount.getInputField()).not.toBeDisabled();
+        expectNoValidationError(elements.pledgeAmount);
     });
 
     it("should disable the form until the project details are loaded", function () {
 
-        $scope.project = { $resolved: false };
-        spyOn(Authentication, 'isLoggedIn').and.returnValue(true);
-        $httpBackend.expectGET('/user/current').respond(200, { budget: 20 });
+        prepareMocks({
+            project: { $resolved: false },
+            isLoggedIn: true,
+            userResponse: { statusCode: 200, body: { budget: 20 } }
+        });
 
         var elements = compileDirective();
         $httpBackend.flush();
 
         expect(elements.slider).toHaveClass('disabled');
-        expect(elements.pledgeAmountField).toBeDisabled();
+        expect(elements.pledgeAmount.getInputField()).toBeDisabled();
+        expectNoValidationError(elements.pledgeAmount);
 
-        angular.copy({ $resolved: true, pledgeGoal: 100, pledgedAmount: 50 }, $scope.project);
+        angular.copy({ $resolved: true, id: 123, pledgeGoal: 100, pledgedAmount: 50 }, $scope.project);
         $scope.$digest();
 
         expect(elements.slider).not.toHaveClass('disabled');
-        expect(elements.pledgeAmountField).not.toBeDisabled();
+        expect(elements.pledgeAmount.getInputField()).not.toBeDisabled();
+        expectNoValidationError(elements.pledgeAmount);
     });
 
     it("should disable the form if the user has no budget", function () {
 
-        $scope.project = { $resolved: true, pledgeGoal: 100, pledgedAmount: 50 };
-        spyOn(Authentication, 'isLoggedIn').and.returnValue(true);
-        $httpBackend.expectGET('/user/current').respond(200, { budget: 0 });
+        prepareMocks({
+            project: { $resolved: true, id: 123, pledgeGoal: 100, pledgedAmount: 50 },
+            isLoggedIn: true,
+            userResponse: { statusCode: 200, body: { budget: 0 } }
+        });
 
         var elements = compileDirective();
         $httpBackend.flush();
 
         expect(elements.slider).toHaveClass('disabled');
-        expect(elements.pledgeAmountField).toBeDisabled();
+        expect(elements.pledgeAmount.getInputField()).toBeDisabled();
+        expectNoValidationError(elements.pledgeAmount);
     });
 
     it("should disable the form if the user is not logged in", function () {
@@ -83,6 +136,55 @@ describe('project pledging form', function () {
         var elements = compileDirective();
 
         expect(elements.slider).toHaveClass('disabled');
-        expect(elements.pledgeAmountField).toBeDisabled();
+        expect(elements.pledgeAmount.getInputField()).toBeDisabled();
+        expectNoValidationError(elements.pledgeAmount);
+    });
+
+    it("should show a validation error message if a too low amount is entered", function () {
+
+        prepareMocks({
+            project: { $resolved: true, id: 123, pledgeGoal: 100, pledgedAmount: 50 },
+            isLoggedIn: true,
+            userResponse: { statusCode: 200, body: { budget: 100 } }
+        });
+
+        var elements = compileDirective();
+        $httpBackend.flush();
+
+        elements.pledgeAmount.getInputField().val('0').trigger('input');
+        expectValidationError(elements.pledgeAmount, 'min');
+        expect(elements.pledgeButton).toBeDisabled();
+    });
+
+    it("should show a validation error message if the entered pledge amount exceeds the pledge goal", function () {
+
+        prepareMocks({
+            project: { $resolved: true, id: 123, pledgeGoal: 100, pledgedAmount: 50 },
+            isLoggedIn: true,
+            userResponse: { statusCode: 200, body: { budget: 200 } }
+        });
+
+        var elements = compileDirective();
+        $httpBackend.flush();
+
+        elements.pledgeAmount.getInputField().val('51').trigger('input');
+        expectValidationError(elements.pledgeAmount, 'max');
+        expect(elements.pledgeButton).toBeDisabled();
+    });
+
+    it("should show a validation error message if the entered pledge amount exceeds the user budget", function () {
+
+        prepareMocks({
+            project: { $resolved: true, id: 123, pledgeGoal: 500, pledgedAmount: 50 },
+            isLoggedIn: true,
+            userResponse: { statusCode: 200, body: { budget: 200 } }
+        });
+
+        var elements = compileDirective();
+        $httpBackend.flush();
+
+        elements.pledgeAmount.getInputField().val('201').trigger('input');
+        expectValidationError(elements.pledgeAmount, 'max');
+        expect(elements.pledgeButton).toBeDisabled();
     });
 });

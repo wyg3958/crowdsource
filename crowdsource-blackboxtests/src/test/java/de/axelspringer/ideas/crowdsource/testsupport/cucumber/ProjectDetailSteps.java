724940bc87c68d68502e53db8feffb010c9ed587
Timo Ulich <timo.ulich@asideas.de>
2015-01-15 11:08:03 +0100

    crowd-106: cucumber refactoring: move add project steps to own class

--- crowdsource-blackboxtests/src/test/java/de/axelspringer/ideas/crowdsource/testsupport/cucumber/ProjectDetailSteps.java
+++ crowdsource-blackboxtests/src/test/java/de/axelspringer/ideas/crowdsource/testsupport/cucumber/ProjectDetailSteps.java
@@ -7,9 +7,6 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import de.axelspringer.ideas.crowdsource.model.presentation.project.Project;
 import de.axelspringer.ideas.crowdsource.testsupport.CrowdSourceTestConfig;
-import de.axelspringer.ideas.crowdsource.testsupport.pageobjects.NavigationBar;
-import de.axelspringer.ideas.crowdsource.testsupport.pageobjects.project.AddProjectConfirmationView;
-import de.axelspringer.ideas.crowdsource.testsupport.pageobjects.project.AddProjectForm;
 import de.axelspringer.ideas.crowdsource.testsupport.pageobjects.project.ProjectDetailPage;
 import de.axelspringer.ideas.crowdsource.testsupport.pageobjects.project.ProjectStatusWidget;
 import de.axelspringer.ideas.crowdsource.testsupport.pageobjects.project.ProjectsPage;
@@ -18,32 +15,17 @@ import de.axelspringer.ideas.crowdsource.testsupport.selenium.WebDriverUtils;
 import de.axelspringer.ideas.crowdsource.testsupport.util.CrowdSourceClient;
 import de.axelspringer.ideas.crowdsource.testsupport.util.UrlProvider;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.hamcrest.Matchers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.startsWith;
 
 @ContextConfiguration(classes = CrowdSourceTestConfig.class)
-public class ProjectSteps {
-
-    @Autowired
-    private NavigationBar navigationBar;
-
-    @Autowired
-    private AddProjectForm addProjectForm;
-
-    @Autowired
-    private AddProjectConfirmationView addProjectConfirmationView;
+public class ProjectDetailSteps {
 
     @Autowired
     private ProjectsPage projectsPage;
@@ -61,8 +43,6 @@ public class ProjectSteps {
     private UrlProvider urlProvider;
 
     private WebDriver webDriver;
-    private String randomProjectTitlePrefix;
-    private String randomProjectShortDescriptionPrefix;
     private Project createdProject;
     private int savedPageYOffset;
 
@@ -77,65 +57,10 @@ public class ProjectSteps {
         createdProject.setTitle("T" + RandomStringUtils.randomAlphanumeric(6));
         createdProject.setShortDescription("Short description " + RandomStringUtils.randomAlphanumeric(16));
         createdProject.setPledgeGoal(25);
-        createdProject.setDescription("This is the project description text." + RandomStringUtils.random(1000, "\nabc"));
+        createdProject.setDescription("This is the project description text." + RandomStringUtils.random(1000, "\nabc").trim());
 
         CrowdSourceClient.AuthToken authToken = crowdSourceClient.authorizeWithDefaultUser();
         createdProject = crowdSourceClient.createProject(createdProject, authToken).getBody();
-    }
-
-    @When("^he clicks on the New Project link in the navigation bar$")
-    public void he_clicks_on_the_New_Project_link_in_the_navigation_bar() throws Throwable {
-        PageFactory.initElements(webDriver, navigationBar);
-        navigationBar.clickNewProject();
-    }
-
-    @Then("^he is redirected to the project creation page$")
-    public void he_is_redirected_to_the_project_creation_page() throws Throwable {
-        PageFactory.initElements(webDriver, addProjectForm);
-        addProjectForm.waitForPageLoad();
-    }
-
-    @When("^he submits the form with valid project data$")
-    public void he_submits_the_form_with_valid_project_data() throws Throwable {
-        PageFactory.initElements(webDriver, addProjectForm);
-
-        randomProjectTitlePrefix = "Title " + RandomStringUtils.randomAlphanumeric(16);
-        randomProjectShortDescriptionPrefix = "Short description " + RandomStringUtils.randomAlphanumeric(16);
-
-        // makes exactly 60 characters, to cause the text to be abbreviated on purpose
-        addProjectForm.setTitle(randomProjectTitlePrefix + " Mmmmm mmmmm mmmmm mmm mmmm, mmmmmmmmm");
-
-        // makes exactly 140 characters, to cause the text to be abbreviated on purpose
-        addProjectForm.setShortDescription(randomProjectShortDescriptionPrefix +
-                " Mmmmm mmmmm mmmmm mmm mmmm, mmmmmmmmmm mmmmmmmmmm mmmmm, mmm mmmm mmmmmm mmmmmm mmmmmm mmmmmmmm mm mmmmmm");
-
-        addProjectForm.setPledgeGoal("25000");
-        addProjectForm.setDescription("Loooong description\nwith newlines");
-        addProjectForm.submit();
-    }
-
-    @Then("^the project creation success page is shown$")
-    public void the_project_creation_success_page_is_shown() throws Throwable {
-        PageFactory.initElements(webDriver, addProjectConfirmationView);
-        addProjectConfirmationView.waitForPageLoad();
-    }
-
-    @When("^he clicks the project overview link$")
-    public void he_clicks_the_project_overview_link() throws Throwable {
-        PageFactory.initElements(webDriver, addProjectConfirmationView);
-        addProjectConfirmationView.clickLinkToProject();
-    }
-
-    @Then("^the project overview page shows the new project$")
-    public void the_project_overview_page_shows_the_new_project() throws Throwable {
-        PageFactory.initElements(webDriver, projectsPage);
-        projectsPage.waitForPageLoad();
-
-        List<Project> projects = projectsPage.getProjects();
-        assertThat(projects, hasItem(Matchers.<Project>hasProperty("title", startsWith(randomProjectTitlePrefix))));
-        assertThat(projects, hasItem(Matchers.<Project>hasProperty("title", endsWith("\u2026")))); // u2026 is the ellipsis unicode character '...'
-        assertThat(projects, hasItem(Matchers.<Project>hasProperty("shortDescription", startsWith(randomProjectShortDescriptionPrefix))));
-        assertThat(projects, hasItem(Matchers.<Project>hasProperty("shortDescription", endsWith("\u2026"))));
     }
 
     @When("^the user clicks on the tile of this published project$")

e7bc251e8099a47e7e525806af8dbb8bfeebef27
Timo Ulich <timo.ulich@asideas.de>
2015-01-17 18:57:15 +0100

    crowd-109: add the written comment to the list of comments already, still reload the comments afterwards (maybe another user wrote a comment in the meanwhile)

--- crowdsource-frontend/src/main/resources/public/app/project/details/project-comments.js
+++ crowdsource-frontend/src/main/resources/public/app/project/details/project-comments.js
@@ -1,6 +1,6 @@
 angular.module('crowdsource')
 
-    .directive('projectComments', function(Comment) {
+    .directive('projectComments', function(Comment, Authentication) {
         return {
             templateUrl: 'app/project/details/project-comments.html',
             controllerAs: 'projectComments',
@@ -16,9 +16,26 @@ angular.module('crowdsource')
                 vm.storeComment = function (comment) {
                     vm.loading = true;
 
-                    Comment.add(vm.project.id, comment).$promise
+                    // work on a copy, or the message in the comment-list
+                    // will be cleared in the .then() callback
+                    var commentCopy = angular.copy(comment);
+                    commentCopy.created = new Date();
+                    commentCopy.userName = Authentication.currentUser.name;
+
+                    Comment.add(vm.project.id, commentCopy).$promise
                         .then(function() {
-                            vm.comments = Comment.getAll(vm.project.id);
+                            vm.comments.push(commentCopy);
+
+                            // clear the text area
+                            vm.newComment.comment = '';
+
+                            // reload comments, do not assign the return value of Comment.getAll
+                            // directly to vm.comments, instead wait for the call to finish
+                            // and then assign it to prevent the list being cleared during loading
+                            Comment.getAll(vm.project.id).$promise
+                                .then(function (reloadedComments) {
+                                    vm.comments = reloadedComments;
+                                });
                         })
                         .finally(function() {
                             vm.loading = false;

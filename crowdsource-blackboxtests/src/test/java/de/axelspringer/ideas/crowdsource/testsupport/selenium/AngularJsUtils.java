e689e93cc42278ccf2f197240ec84c3b4cef9434
Timo Ulich <timo.ulich@asideas.de>
2015-01-17 18:36:38 +0100

    add javadoc do AngularJsUtils.interpolationCompletedOfElementLocated

--- crowdsource-blackboxtests/src/test/java/de/axelspringer/ideas/crowdsource/testsupport/selenium/AngularJsUtils.java
+++ crowdsource-blackboxtests/src/test/java/de/axelspringer/ideas/crowdsource/testsupport/selenium/AngularJsUtils.java
@@ -9,6 +9,15 @@ import java.util.List;
 
 public class AngularJsUtils {
 
+    /**
+     * Use this to wait until angularJs finished interpolating the value of the element located by the given locator.
+     * It is assumed, that the text of the element being watched is interpolated by angularJs with the double curly brackets,
+     * e.g.:
+     *
+     * <some-element>{{ variable }}</some-element>
+     *
+     * This method waits until the given element is found in DOM and it does not start with "{{" and end with "}}"
+     */
     public static ExpectedCondition<Boolean> interpolationCompletedOfElementLocated(final By locator) {
         return new ExpectedCondition<Boolean>() {
             @Override

116518606e558f5cfcaac71b98eed40f29581977
henning.gross <igaffai@gmail.com>
2015-01-17 16:57:09 +0100

    CROWD-109 comments: beginning fe

--- crowdsource-frontend/src/main/resources/public/app/project/details/project-details.js
+++ crowdsource-frontend/src/main/resources/public/app/project/details/project-details.js
@@ -1,12 +1,12 @@
 angular.module('crowdsource')
 
-    .controller('ProjectDetailsController', function($routeParams, $location, Project) {
+    .controller('ProjectDetailsController', function ($routeParams, $location, Project, Comment) {
 
         var vm = this;
 
         vm.project = Project.get($routeParams.projectId);
 
-        vm.project.$promise.catch(function(response) {
+        vm.project.$promise.catch(function (response) {
             if (response.status == 404) {
                 $location.path('/error/notfound');
             }
@@ -15,4 +15,18 @@ angular.module('crowdsource')
             }
         });
 
+        vm.comments = Comment.getAll(vm.project.id);
+
+        vm.storeComment = function (comment) {
+
+            Comment.add(vm.project.id, comment).$promise.catch(function (response) {
+                if (response.status == 404) {
+                    $location.path('/error/notfound');
+                }
+                // TODO: validation errors
+                else {
+                    $location.path('/error/unknown');
+                }
+            });
+        }
     });

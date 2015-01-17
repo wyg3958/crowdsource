bf9b68c8c9ffc4a041c6cbc3533c9850191f845b
Timo Ulich <timo.ulich@asideas.de>
2015-01-17 18:21:01 +0100

    crowd-109: remove length constraint

--- crowdsource-application/src/main/java/de/axelspringer/ideas/crowdsource/model/presentation/Comment.java
+++ crowdsource-application/src/main/java/de/axelspringer/ideas/crowdsource/model/presentation/Comment.java
@@ -5,7 +5,6 @@ import de.axelspringer.ideas.crowdsource.model.presentation.user.UserHelper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 import org.joda.time.DateTime;
 
 import javax.validation.constraints.NotNull;
@@ -20,7 +19,6 @@ public class Comment {
     private String userName;
 
     @NotNull
-    @Length(min = 20, max = 1000, message = "length-constraint-respected")
     private String comment;
 
     public Comment(CommentEntity commentEntity) {

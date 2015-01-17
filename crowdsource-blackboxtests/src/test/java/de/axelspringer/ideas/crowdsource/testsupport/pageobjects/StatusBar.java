69f2c704c38dedf026227b799d53262b99653d8b
Timo Ulich <timo.ulich@asideas.de>
2015-01-17 17:12:28 +0100

    crowd-94: add cucumber test

---
+++ crowdsource-blackboxtests/src/test/java/de/axelspringer/ideas/crowdsource/testsupport/pageobjects/StatusBar.java
@@ -0,0 +1,24 @@
+package de.axelspringer.ideas.crowdsource.testsupport.pageobjects;
+
+import de.axelspringer.ideas.crowdsource.testsupport.selenium.ElementUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatusBar {
+
+    @FindBy(css = "status-bar .row")
+    private WebElement statusBar;
+
+    @FindBy(css = "status-bar .budget .label")
+    private WebElement budgetLabel;
+
+    public boolean isVisible() {
+        return statusBar.isDisplayed();
+    }
+
+    public int getBudget() {
+        return ElementUtils.parseCurrency(budgetLabel);
+    }
+}

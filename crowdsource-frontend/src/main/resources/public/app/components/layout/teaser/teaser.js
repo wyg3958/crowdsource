d2c9f947e3c82a2c1d8ebf1f5c19c9afa2771be2
Timo Ulich <timo.ulich@asideas.de>
2015-01-20 17:12:22 +0100

    crowd-100: show metrics in teaser (remaining time still needs some love)

--- crowdsource-frontend/src/main/resources/public/app/components/layout/teaser/teaser.js
+++ crowdsource-frontend/src/main/resources/public/app/components/layout/teaser/teaser.js
@@ -1,14 +1,39 @@
 angular.module('crowdsource')
 
-    .directive('teaser', function() {
+    .directive('teaser', function ($interval, User, FinancingRound) {
         var directive = {};
 
-        directive.templateUrl = 'app/components/layout/teaser/teaser.html',
         directive.controllerAs = 'teaser';
         directive.bindToController = true;
+        directive.templateUrl = 'app/components/layout/teaser/teaser.html';
 
-        directive.controller = function() {
+        directive.controller = function () {
+            var vm = this;
 
+            vm.userMetrics = User.getMetrics();
+
+            vm.activeRound = FinancingRound.getActive();
+            vm.activeRound.$promise.then(function() {
+                applyRemainingTime();
+                $interval(applyRemainingTime, 1000);
+            });
+
+
+            function applyRemainingTime() {
+                if (!vm.activeRound.$resolved) {
+                    return;
+                }
+
+                vm.remainingTime = getRemainingTime(vm.activeRound.endDate);
+            }
+
+            function getRemainingTime(untilDate) {
+                var now = moment();
+                var end = moment(untilDate);
+
+                var diff = moment.duration(end.diff(now));
+                return diff;
+            }
         };
 
         return directive;

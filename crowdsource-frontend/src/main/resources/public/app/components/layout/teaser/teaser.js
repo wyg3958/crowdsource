f07053c0ff2d2196a57475721c802b6b9947247f
Timo Ulich <timo.ulich@asideas.de>
2015-01-20 17:42:18 +0100

    crowd-100: show a nice remaining time

--- crowdsource-frontend/src/main/resources/public/app/components/layout/teaser/teaser.js
+++ crowdsource-frontend/src/main/resources/public/app/components/layout/teaser/teaser.js
@@ -1,6 +1,40 @@
 angular.module('crowdsource')
 
-    .directive('teaser', function ($interval, User, FinancingRound) {
+    .factory('Metrics', function() {
+        var service = {};
+
+        service.formatRemainingTime = function(untilDate) {
+            var now = moment();
+            var end = moment(untilDate);
+
+            var diff = moment.duration(end.diff(now));
+            if (diff.asSeconds() < 1) {
+                return 'beendet';
+            }
+
+            var remainingTime = '';
+            if (diff.days() > 0) {
+                var days = Math.floor(diff.asDays());
+                if (days > 6) {
+                    return days + ' Tage';
+                }
+                remainingTime += days + 'd ';
+            }
+            if (remainingTime || diff.hours() > 0) {
+                remainingTime += diff.hours() + 'h ';
+            }
+            if (remainingTime || diff.minutes() > 0) {
+                remainingTime += diff.minutes() + 'm ';
+            }
+            remainingTime += diff.seconds() + 's ';
+
+            return remainingTime;
+        };
+
+        return service;
+    })
+
+    .directive('teaser', function ($interval, Metrics, User, FinancingRound) {
         var directive = {};
 
         directive.controllerAs = 'teaser';
@@ -18,21 +52,12 @@ angular.module('crowdsource')
                 $interval(applyRemainingTime, 1000);
             });
 
-
             function applyRemainingTime() {
                 if (!vm.activeRound.$resolved) {
                     return;
                 }
 
-                vm.remainingTime = getRemainingTime(vm.activeRound.endDate);
-            }
-
-            function getRemainingTime(untilDate) {
-                var now = moment();
-                var end = moment(untilDate);
-
-                var diff = moment.duration(end.diff(now));
-                return diff;
+                vm.remainingTime = Metrics.formatRemainingTime(vm.activeRound.endDate);
             }
         };
 

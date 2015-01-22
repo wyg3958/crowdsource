56f5785097b98389713613b06b568caff26cd035
Timo Ulich <timo.ulich@asideas.de>
2015-01-22 11:00:34 +0100

    move footer to its own directive bring footer links to life move navbar to its own directive fix navbar active class

---
+++ crowdsource-frontend/src/main/resources/public/app/components/layout/nav-bar/nav-bar.js
@@ -0,0 +1,25 @@
+angular.module('crowdsource')
+
+    .directive('navBar', function ($location, Authentication) {
+        var directive = {};
+
+        directive.controllerAs = 'nav';
+        directive.bindToController = true;
+        directive.templateUrl = 'app/components/layout/nav-bar/nav-bar.html';
+
+        directive.controller = function () {
+            var vm = this;
+
+            vm.auth = Authentication;
+
+            vm.getClassForMenuItem = function (location) {
+                if ($location.path() == location) {
+                    return 'active';
+                }
+
+                return '';
+            };
+        };
+
+        return directive;
+    });

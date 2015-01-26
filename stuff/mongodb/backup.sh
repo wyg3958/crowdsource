502ac139640dd0eb19c7a449b10009fd92305348
Timo Ulich <timo.ulich@asideas.de>
2015-01-26 15:23:26 +0100

    crowd-29: do not start an interactive tty for docker (not needed)

--- stuff/mongodb/backup.sh
+++ stuff/mongodb/backup.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+# The jenkins job is configured to execute the following shell command:
+#
+# cd stuff/mongodb
+# chmod 400 coreos_rsa
+# ./backup.sh
+
 KEYFILE=coreos_rsa
 HOST=54.194.152.80
 DUMPFILE=mongodump.tar.gz
 
-ssh -t -i ${KEYFILE} core@${HOST} "
-    docker run -it --rm -v /tmp/mongodump:/data/dump mongo:2.6 mongodump --host=172.17.42.1 --db=crowdsource --username=crowdsource --password='sRBfksXzltBgYHxvNUMoSKVHNLsIHHlI1B5Np2S8oyE=' --out=/data/dump
+ssh -i ${KEYFILE} core@${HOST} "
+    docker run --rm -v /tmp/mongodump:/data/dump mongo:2.6 mongodump --host=172.17.42.1 --db=crowdsource --username=crowdsource --password='sRBfksXzltBgYHxvNUMoSKVHNLsIHHlI1B5Np2S8oyE=' --out=/data/dump
     tar czf /tmp/${DUMPFILE} -C /tmp mongodump
 "
 

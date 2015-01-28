72613489b4cd406c58ae0c4d1f009d75294b46c9
Timo Ulich <timo.ulich@asideas.de>
2015-01-28 12:00:11 +0100

    crowd-157: typo

--- crowdsource-frontend/src/main/resources/public/app/project/details/project-pledging-form.js
+++ crowdsource-frontend/src/main/resources/public/app/project/details/project-pledging-form.js
@@ -70,7 +70,7 @@ angular.module('crowdsource')
                         return { type: 'success', message: 'Deine Finanzierung war erfolgreich.' };
                     }
                     if (vm.project.status == 'FULLY_PLEDGED') {
-                        return { type: 'info', message: 'Das Project ist zu 100% finanziert. Eine weitere Finanzierung ist nicht mehr möglich.' };
+                        return { type: 'info', message: 'Das Projekt ist zu 100% finanziert. Eine weitere Finanzierung ist nicht mehr möglich.' };
                     }
                     if (vm.project.status != 'PUBLISHED') {
                         return { type: 'info', message: 'Eine Finanzierung ist erst möglich, wenn das Projekt von einem Administrator veröffentlicht wurde.' };

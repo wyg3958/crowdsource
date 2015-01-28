1b4c110fc000dbc1d3aeac6258a9761ccd3e700a
sweller <stefan.weller@asideas.de>
2015-01-28 11:33:14 +0100

    crowd-143: start refactoring

--- crowdsource-application/src/main/java/de/axelspringer/ideas/crowdsource/service/EmailService.java
+++ crowdsource-application/src/main/java/de/axelspringer/ideas/crowdsource/service/EmailService.java
@@ -1,34 +1,25 @@
 package de.axelspringer.ideas.crowdsource.service;
 
-import de.axelspringer.ideas.crowdsource.model.presentation.user.User;
-import org.apache.velocity.app.VelocityEngine;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.javamail.MimeMessagePreparator;
-import org.springframework.ui.velocity.VelocityEngineUtils;
-
-import java.util.Map;
-
 public class EmailService {
 
-    @Autowired
-    private VelocityEngine velocityEngine;
 
-    @Autowired
-    private JavaMailSender mailSender;
+//    public String parse(String templateName, StandardEvaluationContext context) {
+//        SpelParserConfiguration configuration = new SpelParserConfiguration(true, true);
+//        SpelExpressionParser parser = new SpelExpressionParser(configuration);
+//
+//
+//
+//        return parser.parseExpression(templateString);
+//
+//    }
+//
+//    public static void main(String [] args){
+//        ParserContext context = new TemplateParserContext();
+//        context.setVariable("email", "a@b.de");
+//        context.setVariable("userName", "Otto Meier");
+//        context.setVariable("url", "http://crowd.asideas.de");
+//
+//        new EmailService().parse("", context);
+//    }
 
-    public void send(User user, String subject, final Map<String, Object> templateVariables) {
-        MimeMessagePreparator preparator = mimeMessage -> {
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
-            message.setTo(user.getEmail());
-            message.setFrom("noreply@crowd.asideas.de");
-            message.setSubject(subject);
-
-            String body = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "/email/activation.vm", "ISO-8859", templateVariables);
-
-            message.setText(body, true);
-        };
-        mailSender.send(preparator);
-    }
 }

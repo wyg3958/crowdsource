1b10b4259568644b05ecc5e8c34a6595881460ac
Sebastian Waschnick <sebastian.waschnick@asideas.de>
2015-06-11 19:12:08 +0200

    Fixing ATs because of Property-Sources and @Value

--- crowdsource-integrationtests/src/test/java/de/axelspringer/ideas/crowdsource/testsupport/CrowdSourceTestConfig.java
+++ crowdsource-integrationtests/src/test/java/de/axelspringer/ideas/crowdsource/testsupport/CrowdSourceTestConfig.java
@@ -3,14 +3,25 @@ package de.axelspringer.ideas.crowdsource.testsupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
 
 @Configuration
 @ComponentScan(excludeFilters = @ComponentScan.Filter(Configuration.class))
+@PropertySources({
+        @PropertySource(value = "classpath:application.properties"),
+})
 public class CrowdSourceTestConfig {
 
     @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() throws IOException {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(Boolean.FALSE);
+        propertySourcesPlaceholderConfigurer.setLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:*.properties"));
+        return propertySourcesPlaceholderConfigurer;
     }
 }

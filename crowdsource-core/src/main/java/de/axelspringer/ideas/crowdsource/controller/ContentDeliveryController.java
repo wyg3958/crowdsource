788a3c07bb593bf7877ba13594454d3dfeb2b4aa
Sebastian Waschnick <sebastian.waschnick@asideas.de>
2015-06-11 16:05:02 +0200

    Dynamic Content via Property and Controller

---
+++ crowdsource-core/src/main/java/de/axelspringer/ideas/crowdsource/controller/ContentDeliveryController.java
@@ -0,0 +1,38 @@
+package de.axelspringer.ideas.crowdsource.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/content")
+public class ContentDeliveryController {
+
+    @Value("${de.axelspringer.ideas.crowdsource.content.company.name}")
+    private String companyName;
+
+    @Cacheable
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Map<String, String> content() {
+        Map<String, String> result = new HashMap<>();
+        result.put("de.axelspringer.ideas.crowdsource.content.company.name", companyName);
+        return result;
+    }
+
+    public static class Entry {
+        public String key;
+        public String value;
+
+        public Entry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}

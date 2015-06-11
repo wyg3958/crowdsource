54c64b6d3a04cf414780aa55a7112008dc9e9690
Sebastian Waschnick <sebastian.waschnick@asideas.de>
2015-06-11 13:31:01 +0200

    Make Mail-Templates configurable

---
+++ crowdsource-core/src/test/java/de/axelspringer/ideas/crowdsource/config/mail/MailTemplateConfigTest.java
@@ -0,0 +1,56 @@
+package de.axelspringer.ideas.crowdsource.config.mail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.expression.Expression;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MailTemplateConfigTest {
+
+    @InjectMocks
+    private MailTemplateConfig mailTemplateConfig;
+
+    @Test
+    public void toDefaultTemplatePath() throws Exception {
+        String defaultTemplatePath = mailTemplateConfig.toDefaultTemplatePath("sometemplate.template");
+        assertThat(defaultTemplatePath, is(equalTo("sometemplate.template.default")));
+    }
+
+    @Test
+    public void activationEmailTemplate() {
+        Expression expression = mailTemplateConfig.activationEmailTemplate();
+        assertThat(expression, is(not(nullValue())));
+    }
+
+    @Test
+    public void newProjectEmailTemplate() {
+        Expression expression = mailTemplateConfig.newProjectEmailTemplate();
+        assertThat(expression, is(not(nullValue())));
+    }
+
+    @Test
+    public void passwordForgottenEmailTemplate() {
+        Expression expression = mailTemplateConfig.passwordForgottenEmailTemplate();
+        assertThat(expression, is(not(nullValue())));
+    }
+
+    @Test
+    public void projectPublishedEmailTemplate() {
+        Expression expression = mailTemplateConfig.projectPublishedEmailTemplate();
+        assertThat(expression, is(not(nullValue())));
+    }
+
+    @Test
+    public void projectRejectedEmailTemplate() {
+        Expression expression = mailTemplateConfig.projectRejectedEmailTemplate();
+        assertThat(expression, is(not(nullValue())));
+    }
+}

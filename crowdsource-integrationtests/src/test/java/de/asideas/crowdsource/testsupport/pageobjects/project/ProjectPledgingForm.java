f70279df24f30f7d27d55f18e5b7fc672a9c65e8
Thomas Wendzinski <thomas.wendzinski@asideas.de>
2015-11-09 15:22:10 +0100

    Stabilizes test - 3

--- crowdsource-integrationtests/src/test/java/de/asideas/crowdsource/testsupport/pageobjects/project/ProjectPledgingForm.java
+++ crowdsource-integrationtests/src/test/java/de/asideas/crowdsource/testsupport/pageobjects/project/ProjectPledgingForm.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -118,6 +119,7 @@ public class ProjectPledgingForm {
         return d.findElement(By.cssSelector(cssSelector));
     }
     private void makeSureDataLoaded(){
+        PageFactory.initElements(webDriverProvider.provideDriver(), this);
         try{
             wait.until( d -> {
                 try{

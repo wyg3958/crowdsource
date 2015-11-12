8eb1dd5f958f05639ec2ec848f0090cc254c44b6
Stefan Bieler <stefan.bieler@asideas.de>
2015-11-12 13:31:16 +0100

    CROWD-40: Stage dependent tracking properties. Move jire issue collector script-reference into footer of crowdsource project.

--- crowdsource-integrationtests/src/test/java/de/asideas/crowdsource/IndexIT.java
+++ crowdsource-integrationtests/src/test/java/de/asideas/crowdsource/IndexIT.java
@@ -8,16 +8,38 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.EnvironmentTestUtils;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.mock.env.MockPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebIntegrationTest
 @SpringApplicationConfiguration(classes = {CrowdSourceExample.class, CrowdSourceTestConfig.class})
 public class IndexIT {
+
+    public static final String DEFAULT_TEST_PROPERTY_SOURCE = "testPropsSource";
+
+    private MockPropertySource testPropertySource;
 
     @Autowired
     private UrlProvider urlProvider;
@@ -30,9 +52,20 @@ public class IndexIT {
 
     private WebDriver webDriver;
 
+    @Autowired
+    ConfigurableEnvironment env;
+
     @Before
     public void initDriver() {
         webDriver = webDriverProvider.provideDriver();
+
+        testPropertySource = new MockPropertySource(DEFAULT_TEST_PROPERTY_SOURCE);
+        // Overlay properties, set by tests.
+        if (env.getPropertySources().contains(DEFAULT_TEST_PROPERTY_SOURCE)) {
+            env.getPropertySources().replace(DEFAULT_TEST_PROPERTY_SOURCE, testPropertySource);
+        } else {
+            env.getPropertySources().addFirst(testPropertySource);
+        }
     }
 
     @After
@@ -44,5 +77,46 @@ public class IndexIT {
     public void testIndexPage() {
         webDriver.get(urlProvider.applicationUrl() + "/");
         wait.until(driver -> "CrowdSource - Projekte".equals(driver.getTitle()));
+
+        List<WebElement> trackingParentTag = webDriver.findElements(By.id("piwiktracking"));
+        assertThat(trackingParentTag, hasSize(0));
     }
+
+    @Test
+    public void testIndexPageWithTrackingEnabled() {
+        String expectedTrackingUrl = "http://trackingrulz.crowdsource.de";
+        String expectedSiteId = "1234567890987654321";
+        testPropertySource.withProperty("de.asideas.crowdsource.tracking.piwik.trackurl", expectedTrackingUrl)
+                    .withProperty("de.asideas.crowdsource.tracking.piwik.siteid", expectedSiteId);
+
+        webDriver.get(urlProvider.applicationUrl() + "/");
+        wait.until(driver -> "CrowdSource - Projekte".equals(driver.getTitle()));
+
+        List<WebElement> trackingParentTag = webDriver.findElements(By.id("piwiktracking"));
+
+        assertThat(trackingParentTag, hasSize(1));
+        assertThat(trackingParentTag.get(0).getTagName(), is("script"));
+
+        String trackingJs = trackingParentTag.get(0).getAttribute("innerHTML");
+
+        assertThat(trackingJs, containsString(expectedTrackingUrl));
+        assertThat(trackingJs, containsString(expectedSiteId));
+    }
+
+    @Test
+    public void testIndexPageWithJiraIssueCollectorEnabled() {
+        String jicTestUrl = "http://jirarulz.crowdsource.de/";
+        testPropertySource.withProperty("de.asideas.crowdsource.jira.issuecollector.url", jicTestUrl);
+
+        webDriver.get(urlProvider.applicationUrl() + "/");
+        wait.until(driver -> "CrowdSource - Projekte".equals(driver.getTitle()));
+
+        List<WebElement> jicParentTag = webDriver.findElements(By.id("jiraissuecollector"));
+
+        assertThat(jicParentTag, hasSize(1));
+        assertThat(jicParentTag.get(0).getTagName(), is("script"));
+
+        assertThat(jicParentTag.get(0).getAttribute("src"), is(jicTestUrl));
+    }
+
 }

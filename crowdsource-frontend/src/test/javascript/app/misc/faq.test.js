3ad0c3876e87511f642c3a27d956a39c19ac9f1f
Thomas Wendzinski <thomas.wendzinski@asideas.de>
2015-11-12 13:27:06 +0100

    CROWD-40 Enhances test and adds tracking documentation

--- crowdsource-frontend/src/test/javascript/app/misc/faq.test.js
+++ crowdsource-frontend/src/test/javascript/app/misc/faq.test.js
@@ -29,7 +29,13 @@ describe('faq', function () {
         expect(view.find("accordion-item .accordion-navigation a")).toHaveAttr("analytics-category", "UserActions");
         expect(view.find("accordion-item .accordion-navigation a")).toHaveAttr("analytics-event", "FaqEntryOpened");
         expect(view.find("accordion-item .accordion-navigation a")).toHaveAttr("analytics-label", "faq_entry");
-        expect(view.find("accordion-item .accordion-navigation a")).toHaveAttr("analytics-value");
+
+        var faqItemParents = view.find("accordion-item");
+        var faqItemLinks = view.find("accordion-item .accordion-navigation a");
+        for(var idx = 0; idx < faqItemParents.length; idx ++){
+            var expVal = faqItemParents[idx].getAttribute("title");
+            expect(faqItemLinks[idx]).toHaveAttr("analytics-value", expVal);
+        }
 
     });
 
